f180e6fe5dc0417184444a3cf28a1574c3921b5b
Christian Broberg <cb@webhouse.dk>
2017-11-13 22:28:38 +0100

    Reinstate cwm to call setHelpID and rename SetHelpID to camelcase (it's a method not a component)

--- src/components/Workspace/Workspace.js
+++ src/components/Workspace/Workspace.js
@@ -3,6 +3,12 @@ import { WorkspaceDiv } from './WorkspaceStyles'
 
 class Workspace extends Component {
 	
+	componentWillMount() {
+		if (this.props.helpID) {
+			this.props.SetHelpID(this.props.helpID)
+		}
+	}
+
 	render() {
 		return (
 			<WorkspaceDiv style={this.props.style}>
