a7f52a98868967440b8a20f30eb5a3b6f1ddd148
Christian Broberg <christianbroberg@CB-MacBook-Pro.lan>
2017-12-18 23:00:47 +0100

    Linting Tabs in NotFound

--- src/components/AppContainer/NotFound.js
+++ src/components/AppContainer/NotFound.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react'
 import Workspace from '../Workspace/Workspace'
 
 export default class NotFound extends Component {
-    render() {
-        return (
-            <Workspace>
-                <h2>
+	render() {
+		return (
+			<Workspace>
+				<h2>
                     404
-                </h2>
-            </Workspace>
-        )
-    }
+				</h2>
+			</Workspace>
+		)
+	}
 }
