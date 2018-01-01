713f24b972dd8c63b92330f0cc16c499afb78bb1
Christian Broberg <cb@webhouse.dk>
2018-01-01 13:47:39 +0100

    Changed alignment in Header

--- src/components/Header/HeaderStyles.js
+++ src/components/Header/HeaderStyles.js
@@ -12,7 +12,8 @@ export const HeaderDiv = styled.div`
 	width: 100vw;
 	height: ${props => props.theme.header.height ? props.theme.header.height : '80px'};
 	align-items: center;
-	justify-content: ${props => props.quicknav ? 'center' : 'space-between'};
+	justify-content: ${props => props.quicknav && 'center'};
+	/* justify-content: ${props => props.quicknav ? 'center' : 'space-between'}; */
 	background-color: ${props => props.theme.header.background};
 	color: ${props => props.theme.header.color};
 	${media.giant`height:80px;`}
